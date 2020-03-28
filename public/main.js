9b110106c446a7ed378b07124e653f512c07b574
miguel <miguel.ramos@avature.net>
2020-03-28 23:20:54 +0100

    disabled cors in dev env

--- public/main.js
+++ public/main.js
@@ -6,10 +6,17 @@ const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
 
+let enableCORS;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    enableCORS = false
+} else {
+    enableCORS = true
+}
+
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: {webSecurity: enableCORS}});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 }
--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import { ListGroup } from 'react-bootstrap';
-import racks from './mock-api/racks.json';
-import sites from './mock-api/sites.json'
+// import racks from './mock-api/racks.json';
+// import sites from './mock-api/sites.json'
 
 
 class App extends Component {
 
   componentDidMount() {
-    // axios.get('https://netboxqa.xcade.net/api/dcim/racks/', {
-    //   headers: {
-    //     'Authorization': 'Token 467c528e78698047e8af1557597712c7e5073ad6'
-    //   }
-    // })
-    // .then(response => console.log(response.data))
-    // .catch(err => console.log(err))
-
-
+    axios.get('https://netboxqa.xcade.net/api/dcim/racks/', {
+      headers: {
+        'Authorization': 'Token 467c528e78698047e8af1557597712c7e5073ad6'
+      }
+    })
+    .then(response => console.log(response.data))
+    .catch(err => console.log(err))
   }
 
   render() {
@@ -32,8 +30,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Racks</h1>
         <ListGroup>
-          <ListGroup.Item>Crasy justo odio</ListGroup.Item>
+          <ListGroup.Item>Cras justo odio</ListGroup.Item>
           <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
           <ListGroup.Item>Morbi leo risus</ListGroup.Item>
           <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
