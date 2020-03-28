369efbba9acccc9432d09ffbe88c9d231e168cb5
miguel <miguel.ramos@avature.net>
2020-03-28 22:48:32 +0100

    harcoded call to api (not working - CORS), mock json provided

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get('https://netboxqa.xcade.net/api/dcim/racks/', {
+      headers: {
+        'Authorization': 'Token 467c528e78698047e8af1557597712c7e5073ad6'
+      }
+    })
+    .then(response => console.log(response.data))
+    .catch(err => console.log(err))
+  }
+
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
@@ -15,6 +28,7 @@ class App extends Component {
         </p>
       </div>
     );
+
   }
 }
 
